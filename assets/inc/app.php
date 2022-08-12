680385f0c8569593c9a9e32afae9d1e1cdbe9942
Killian V <killian.vaillant@gmail.com>
2022-08-12 14:41:06 +0200

    Cancel change PHP to HTML

--- assets/inc/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-$curl= curl_init();
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
-curl_setopt($curl, CURLOPT_URL, 'https://api.mineaurion.com/query');
-$res = curl_exec($curl);
-curl_close($curl);
-$jo = json_decode($res);
-var_dump($res);
-die();
