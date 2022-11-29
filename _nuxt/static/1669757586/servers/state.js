aa7c4589a20e753ca50f133a8c4ee00cd7066c33
Yann151924 <Yann151924@users.noreply.github.com>
2022-11-29 21:33:08 +0000

    deploy: f78c71c524707956b4efa7710d3652008978844a

---
+++ _nuxt/static/1669757586/servers/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FWebsite_new\u002F_nuxt\u002Fstatic\u002F1669757586",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservers",config:{_app:{basePath:"\u002FWebsite_new\u002F",assetsPath:"\u002FWebsite_new\u002F_nuxt\u002F",cdnURL:a}}}}(null));
