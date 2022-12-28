61cca95fee6227186c96bc438892ff519e5894e1
killianvlt <killian.vaillant@gmail.com>
2022-12-28 10:06:35 +0100

    Add social network links & website links

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,37 +10,24 @@
       </div>
     </div>
     <div class="lg:h-52 bg-blue w-full">
-      <div class="container mx-auto grid lg:grid-flow-col items-center justify-center lg:justify-around">
+      <div class="container mx-auto flex flex-col md:flex-row items-center justify-center lg:justify-between pt-10">
         <ul class="flex flex-col">
-          <h3>Title</h3>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
+          <h3 class="text-lg uppercase mb-4">Liens utiles</h3>
+          <li><a href="https://forum.mineaurion.com">Forum</a></li>
+          <li><a href="/servers">Serveurs</a></li>
+          <li><a href="https://shop.mineaurion.com">Boutique</a></li>
         </ul>
         <ul class="flex flex-col">
-          <h3>Title</h3>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-        </ul>
-        <ul class="flex flex-col">
-          <h3>Title</h3>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
+          <h3 class="text-lg uppercase mb-4">Retrouvez nous sur :</h3>
+          <div class="flex flex-col md:flex-row">
+            <li><a href="https://twitter.com/mineaurion" target="_blank" title="Accéder à notre compte Twitter"><img src="~/assets/img/twitter.svg" alt="" class="h-16 w-16 bg-dark-blue rounded-xl"></a></li>
+            <li><a href="https://www.youtube.com/user/Mineaurion" title="Accéder à notre compte Youtube" target="_blank" class=""><img src="~/assets/img/youtube.svg" alt="" class=" ml-2 h-16 w-16 bg-dark-blue rounded-xl"></a></li>
+            <li><a href="https://mineaurion.com/discord" target="_blank"><img class="ml-2 rounded-xl" src="https://discordapp.com/api/guilds/99116990634950656/widget.png?style=banner2"></a></li>
+          </div>
         </ul>
         <ul class="flex flex-col">
           <h3 class="text-lg uppercase mb-4">Nos partenaires :</h3>
-          <li class="w-max"><a href="https://www.twitch.tv/mukamultigaming/" target="_blank"><img class="brightness-75 hover:brightness-100 duration-100" src="~/assets/img/muka.png"></a></li>
+          <li class="w-max"><a href="https://www.twitch.tv/mukamultigaming/" target="_blank" title="Accéder à la chaîne Twitch de mukamultigaming"><img class="brightness-75 hover:brightness-100 duration-100" src="~/assets/img/muka.png" alt="Accéder à la chaîne twitch de mukamultigaming"></a></li>
         </ul>
       </div>
     </div>
